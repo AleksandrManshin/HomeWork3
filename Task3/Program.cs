9c9c6f0975afba0389719abd09cb8485f90b6a30
Aleksandr <maivan007@yandex.ru>
2022-07-04 18:26:01 +0400

    Add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= number)
+{
+    Console.Write(Math.Pow(count, 3) + " ");
+    count++;
+}
+Console.WriteLine();
