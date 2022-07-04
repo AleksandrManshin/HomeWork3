f32c1ce0e20f6f1abf5971d755af6995fa4dd64a
Aleksandr <maivan007@yandex.ru>
2022-07-04 18:22:42 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите координаты двух точек");
+Console.WriteLine("Координаты первой точки");
+Console.Write("X1:");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1:");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1:");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координаты второй точки");
+Console.Write("X2:");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2:");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2:");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
+Console.WriteLine("Расстояние между двумя точками в пространстве" + "{0: 0.00}",result);
