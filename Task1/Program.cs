42d3dc2a71b2458558d7840039ea4a7cdb51f495
Aleksandr <maivan007@yandex.ru>
2022-07-04 18:19:53 +0400

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+string number = Console.ReadLine();
+if(number.Length != 5)
+{
+Console.WriteLine($"Число {number} не является пятизначным");
+}
+else if(number[0] == number[4] & number[1] == number[3])
+{
+Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+Console.WriteLine($"Число {number} не является палиндромом");
+}
